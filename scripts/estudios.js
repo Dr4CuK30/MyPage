877457c70d1b6cb5a9bae61c0c284cf4d7a655a8
David Saldarriaga <david.saldarriaga@rappi.com>
2024-01-28 21:29:21 -0500

    feat: :lipstick: year update

--- scripts/estudios.js
+++ scripts/estudios.js
@@ -1,62 +1,65 @@
 var estudios = [
-	{
-		titulo: 'Angular: De cero a experto (Edición 2021)',
-		lugar: 'Udemy',
-		url: 'assets/Imágenes/educacion/UdemyLogo.png',
-		descripcion:
-			'Componentes, directivas, servicios, mapas, gráficas, JWT, autenticación, despliegues, mongo, Git, GitHub y mucho más.',
-		type: 'Web',
-		style: 'danger',
-		fecha: 'Julio 2021',
-		diploma: 'https://www.udemy.com/certificate/UC-ee6558cb-d02f-4e0e-9ae4-934de4393265/',
-	},
-	{
-		titulo: 'Diplomado Habilidades de Programación',
-		lugar: 'Universidad Nacional de Colombia',
-		url: 'assets/Imágenes/educacion/DiplomadoUnal.png',
-		descripcion:
-			'Programa de conocimiento académico en habilidades de programación MISIÓN TIC 2022.',
-		type: 'Programación',
-		style: 'warning',
-		fecha: 'Diciembre 2020',
-		diploma: 'https://drive.google.com/file/d/1bskxXEEWKczhu7vd2HU0GqgmFStfpfOt/view?usp=sharing',
-	},
-	{
-		titulo: 'Curso Profesional de Javascript',
-		lugar: 'Platzi',
-		url: 'assets/Imágenes/educacion/JavascriptPro.png',
-		descripcion:
-			'Curso dedicado a la profundización de conocimientos en Javascript y Typescript y aplicación de buenas practicas en dichos lenguajes.',
-		type: 'Programación',
-		style: 'warning',
-		fecha: 'Marzo 2021',
-		diploma: 'https://drive.google.com/file/d/1e7rp6vf6_BlALveryfJAb1M4hlibMjYn/view?usp=sharing',
-	},
-	{
-		titulo: 'Ingeniería de Sistemas',
-		lugar: 'Universidad Distrital Francisco Jose de Caldas',
-		url: 'assets/Imágenes/educacion/Distrital.png',
-		descripcion: `Pregrado de la facultad de ingeniería, actualmente en 5 semestre y miembro activo de ACM como tutor del grupo de trabajo GIWEB.`,
-		type: 'Ingeniería',
-		style: 'primary',
-		fecha: 'Cursando actualmente',
-	},
-	{
-		titulo: 'Angular (Intermediate) Certificate',
-		lugar: 'HackerRank',
-		url: 'assets/Imágenes/educacion/Angular-Hack.png',
-		descripcion: `It covers topics like Routing, NgModules, Observables for data transmission and event handling, Dependency Injections, and APIs.`,
-		type: 'Web',
-		style: 'danger',
-		fecha: 'Junio 2021',
-		diploma: 'https://www.hackerrank.com/certificates/4907ea1e8da3',
-	},
+  {
+    titulo: "Angular: De cero a experto (Edición 2021)",
+    lugar: "Udemy",
+    url: "assets/images/educacion/UdemyLogo.png",
+    descripcion:
+      "Componentes, directivas, servicios, mapas, gráficas, JWT, autenticación, despliegues, mongo, Git, GitHub y mucho más.",
+    type: "Web",
+    style: "danger",
+    fecha: "Julio 2021",
+    diploma:
+      "https://www.udemy.com/certificate/UC-ee6558cb-d02f-4e0e-9ae4-934de4393265/",
+  },
+  {
+    titulo: "Diplomado Habilidades de Programación",
+    lugar: "Universidad Nacional de Colombia",
+    url: "assets/images/educacion/DiplomadoUnal.png",
+    descripcion:
+      "Programa de conocimiento académico en habilidades de programación MISIÓN TIC 2022.",
+    type: "Programación",
+    style: "warning",
+    fecha: "Diciembre 2020",
+    diploma:
+      "https://drive.google.com/file/d/1bskxXEEWKczhu7vd2HU0GqgmFStfpfOt/view?usp=sharing",
+  },
+  {
+    titulo: "Curso Profesional de Javascript",
+    lugar: "Platzi",
+    url: "assets/images/educacion/JavascriptPro.png",
+    descripcion:
+      "Curso dedicado a la profundización de conocimientos en Javascript y Typescript y aplicación de buenas practicas en dichos lenguajes.",
+    type: "Programación",
+    style: "warning",
+    fecha: "Marzo 2021",
+    diploma:
+      "https://drive.google.com/file/d/1e7rp6vf6_BlALveryfJAb1M4hlibMjYn/view?usp=sharing",
+  },
+  {
+    titulo: "Ingeniería de Sistemas",
+    lugar: "Universidad Distrital Francisco Jose de Caldas",
+    url: "assets/images/educacion/Distrital.png",
+    descripcion: `Pregrado de la facultad de ingeniería, actualmente en 5 semestre y miembro activo de ACM como tutor del grupo de trabajo GIWEB.`,
+    type: "Ingeniería",
+    style: "primary",
+    fecha: "Cursando actualmente",
+  },
+  {
+    titulo: "Angular (Intermediate) Certificate",
+    lugar: "HackerRank",
+    url: "assets/images/educacion/Angular-Hack.png",
+    descripcion: `It covers topics like Routing, NgModules, Observables for data transmission and event handling, Dependency Injections, and APIs.`,
+    type: "Web",
+    style: "danger",
+    fecha: "Junio 2021",
+    diploma: "https://www.hackerrank.com/certificates/4907ea1e8da3",
+  },
 ];
 
 function htmlGenerator() {
-	var html = ``;
-	for (let estudio of estudios) {
-		html += `
+  var html = ``;
+  for (let estudio of estudios) {
+    html += `
         <li class="glide__slide">
             <div class="card bg-p">
                 <img class="card-img-top" src="${estudio.url}" alt="${estudio.titulo}">
@@ -70,21 +73,21 @@ function htmlGenerator() {
                     </div>
                     <p class="card-text mt-2">${estudio.descripcion}</p>
                 `;
-		if (estudio.diploma) {
-			html += `<div class="text-center">
+    if (estudio.diploma) {
+      html += `<div class="text-center">
             <a href="${estudio.diploma}" target="_blank" class="btn btn-diploma">Ver Certificado</a>
         </div>`;
-		}
-		html += `</div>
+    }
+    html += `</div>
                     </div>
                 </li>`;
-	}
-	return html;
+  }
+  return html;
 }
 
 function loadEstudios() {
-	const estudios_div = document.getElementById('estudios');
-	estudios_div.innerHTML = htmlGenerator();
+  const estudios_div = document.getElementById("estudios");
+  estudios_div.innerHTML = htmlGenerator();
 }
 
 loadEstudios();
